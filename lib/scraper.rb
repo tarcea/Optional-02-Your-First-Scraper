8e651ad4483d95f4b38e6ce89503c18f78a451a8
Gheorghe Tarcea <tarcea@gmail.com>
2020-04-27 15:27:29 +0200

    play with scraping in ruby

---
+++ lib/scraper.rb
@@ -0,0 +1,22 @@
+require "open-uri"
+require "nokogiri"
+
+def good_reads(search_term)
+  # TODO: return an array of Antiques found of Craigslist for this `city`.
+  greads = open("https://www.elefant.ro/search?SearchTerm=#{search_term}").read
+  doc = Nokogiri::HTML(greads)
+  doc.search('.product-list-item').each_with_index do |element, index|
+  puts index + 1
+  puts element
+  end
+
+end
+
+good_reads("ewan")
+
+# html_content = open('https://www.etsy.com/search?q=wallet').read
+# doc = Nokogiri::HTML(html_content)
+
+# doc.search('.responsive-listing-grid .v2-listing-card__info .text-body').each_with_index do |element, index|
+#   puts "#{index + 1}. #{element.text.strip}"
+# end
